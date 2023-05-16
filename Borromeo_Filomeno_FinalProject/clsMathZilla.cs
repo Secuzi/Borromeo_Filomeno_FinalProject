57480a97167f1972e3fa6730a264c4f63919ed60
Secuzi <secre2k@gmail.com>
2023-05-16 16:07:05 +0800

    Needs indication based on who's turn is it.

--- Borromeo_Filomeno_FinalProject/clsMathZilla.cs
+++ Borromeo_Filomeno_FinalProject/clsMathZilla.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Borromeo_Filomeno_FinalProject
 {
@@ -15,7 +17,11 @@ namespace Borromeo_Filomeno_FinalProject
         public int p2_ans;
         public int Num1;
         public int Num2;
+        public int P1Score;
+        public int P2Score;
         public int Result;
+        public bool P1Turn;
+        public bool P2Turn;
         public bool Playing;
 
         public clsMathZilla()
@@ -25,38 +31,76 @@ namespace Borromeo_Filomeno_FinalProject
             Game_Mode = null;
             Playing = false;
         }
-        public void Addition_Problem()
+
+
+        public bool CheckPlayer1Answer(clsMathZilla player)
+        {
+            if (player.p1_ans == Result)
+                return true;
+            else return false;
+
+        }
+
+        public bool CheckPlayer2Answer(clsMathZilla player)
+        {
+            if (player.p2_ans == Result)
+                return true;
+            else return false;
+
+        }
+
+
+
+        public void Addition_Problem(Label problem)
         {
             // call to create new problem
             Random random = new Random();
             Num1 = random.Next(1, 100);
             Num2 = random.Next(1, 100);
             Result = Num1 + Num2;
+
+            problem.Text = $"{Num1} + {Num2}";
+
+            
         }
 
-        public void Subtraction_Problem()
+        public void Subtraction_Problem(Label problem)
         {
             Random random = new Random();
             Num1 = random.Next(50, 100);
             Num2 = random.Next(1, 50);
             Result = Num1 - Num2;
+            problem.Text = $"{Num1} - {Num2}";
         }
 
-        public void Multiplication_Problem()
+        public void Multiplication_Problem(Label problem)
         {
             Random random = new Random();
             Num1 = random.Next(1, 10);
             Num2 = random.Next(1, 10);
             Result = Num1 * Num2;
+            problem.Text = $"{Num1} * {Num2}";
         }
 
-        public void Division_Problem()
+        public void Division_Problem(Label problem)
         {
             Random random = new Random();
             Num1 = random.Next(50, 100);
             Num2 = random.Next(1, 50);
             Result = Num1 / Num2;
+            problem.Text = $"{Num1} / {Num2}";
         }
+
+        public int ShowClue()
+        {
+            Random rnd = new Random();     
+            char[] digits = Result.ToString().ToCharArray();
+            int digit = digits[rnd.Next(0, digits.Length)];
+            return digit;
+
+
+        }
+
 
         public void Game_Start()
         {
@@ -64,7 +108,7 @@ namespace Borromeo_Filomeno_FinalProject
             {
                 while (Playing)
                 {
-                    Addition_Problem();
+                    
                 }
             }
             else if (Game_Mode == "Subtraction")
